4ae3c06d9f495a030c63a10e616f775c5ff7ffac
Kayyy-y <judith727@naver.com>
2024-01-10 15:00:34 +0900

    upload cleaning file data set cleaning

--- 0. Cleaning.R
+++ 0. Cleaning.R
@@ -12,7 +12,7 @@ library(stringr)
 # Account Verification
 # -----------------------------------------------------------------------------------
 gs4_auth()
-1
+2
 
 
 
@@ -30,21 +30,27 @@ out.sheet <- gs4_get(wdir)
 # -----------------------------------------------------------------------------------
 
 ### Data;
-setwd("C:/Users/lenovo/Desktop/Game Recommendations on Steam/0. Data/0. Source")
+setwd("./0. Data/0. Source")
 game <- read.csv("games.csv")
 recom <- read.csv("recommendations.csv")
 users <- read.csv("users.csv")
 
+# -----------------------------------------------------------------------------------
+# Check data - game
+# -----------------------------------------------------------------------------------
 
 ### games
 game %>% head()
 game %>% names()
+str(game)
 
-game <- game %>% 
-  mutate(win = ifelse(win == "false", FALSE, TRUE)) %>% 
-  mutate(mac = ifelse(mac == "false", FALSE, TRUE)) %>% 
-  mutate(linux = ifelse(linux == "false", FALSE, TRUE)) %>% 
-  mutate(steam_deck = ifelse(steam_deck == "false", FALSE, TRUE))
+game <- game %>%
+  mutate(win = ifelse(win == "false", FALSE, TRUE)) %>%
+  mutate(mac = ifelse(mac == "false", FALSE, TRUE)) %>%
+  mutate(linux = ifelse(linux == "false", FALSE, TRUE)) %>%
+  mutate(steam_deck = ifelse(steam_deck == "false", FALSE, TRUE)) %>%
+  mutate(discount_per = as.double(discount)) %>%
+  mutate(date_release = as.Date(date_release))
 
 game_summary <- game %>% summary()
 
@@ -53,10 +59,41 @@ game_summary <- game %>% summary()
 game %>% 
   select(app_id) %>% unique() %>% count()
 
+# duplicated? - title(x)
+game %>% 
+  select(title) %>% 
+  group_by(title) %>% 
+  mutate(n = n()) %>% head(10)
+
+# NA check
+#NA check
+table(is.na(game))
+
+
+## mutate(discount)
+# (old)         (new)
+# discount -> discount_per
+# (new col)
+# discount or not -> discount
+
+game <- game %>% 
+  mutate(discount = ifelse(discount_per > 0, TRUE, FALSE))
+
+
+
+# -----------------------------------------------------------------------------------
+# Check data - recom
+# -----------------------------------------------------------------------------------
 
 ### recom
 recom %>% head()
 recom %>% names()
+str(recom)
+
+
+recom <- recom %>%
+  mutate(is_recommended = ifelse(is_recommended == "false", FALSE, TRUE)) %>%
+  mutate(date = as.Date(date))
 
 recom_summary <- recom %>% summary()
 
@@ -64,10 +101,26 @@ recom_summary <- recom %>% summary()
 recom %>% 
   select(review_id) %>% unique() %>% count()
 
+#duplicate check
+recom %>%  
+  select(user_id) %>% 
+  group_by(user_id) %>% 
+  mutate(n=n()) %>% 
+  arrange(desc(n)) %>% unique() %>% head()
+
+#NA check
+table(is.na(recom))
+
+
+
+# -----------------------------------------------------------------------------------
+# Check data - users
+# -----------------------------------------------------------------------------------
 
 ### users
 users %>% head()
 users %>% names()
+str(users)
 
 users_summary <- users %>% summary()
 
@@ -75,33 +128,18 @@ users_summary <- users %>% summary()
 users %>% 
   select(user_id) %>% unique() %>% count()
 
-
-# -----------------------------------------------------------------------------------
-# Check
-# -----------------------------------------------------------------------------------
-
-recom %>% 
-  left_join(users) %>% 
-  filter(products == na) %>% count()
-
-recom %>% select(user_id) %>% unique() %>% count()
-recom %>% select(app_id) %>% unique() %>% count()
+#NA check
+table(is.na(users))
 
 
 
 # -----------------------------------------------------------------------------------
-# Output
+# Data Output
 # -----------------------------------------------------------------------------------
 
-# data03 %>%
-#   arrange(이름) %>%
-#   select(-index1) %>%
-#   write_sheet(out.sheet,
-#               sheet = '8.수료생FW3')
+setwd("..")
+setwd("./1. Cleaned_data")
+write.csv(game, "./games_edit.csv", row.names = FALSE)
+write.csv(recom, "./recommendations_edit.csv", row.names = FALSE)
+write.csv(users, "./users_edit.csv", row.names = FALSE)
 
-
-data05 %>%
-  arrange(이름) %>%
-  filter(KEY != "노션 - 이력서 공유") %>%
-  write_sheet(out.sheet,
-              sheet = '6.수료생FW2')
